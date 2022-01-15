f06382d2fb8760937520ec3f4f1a16a68614ce98
kartikkhk <kartik.rawat@smallcase.com>
2022-01-15 15:02:57 +0530

    add: queueManager

---
+++ index.js
@@ -0,0 +1,18 @@
+function QueueManager(queue) {
+  this._emitter = new EventEmitter();
+  const self = this;
+  queue.drain(() => {
+    self._emitter.emit("toggle");
+  });
+}
+
+QueueManager.prototype.pause = function () {
+  const self = this;
+  return new Promise((resolve) => {
+    self._emitter.on("toggle", () => resolve(true));
+  });
+};
+
+QueueManager.prototype.resume = function () {
+  this._emitter.emit("toggle");
+};
