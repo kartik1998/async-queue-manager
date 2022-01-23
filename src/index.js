d8a5ac9432fe3503a2ff03ee85dcc256141b1f43
kartik1998 <kartikrawat61@gmail.com>
2022-01-23 14:44:23 +0530

    refactor: add state definitions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const EventEmitter = require("events");
 function QueueManager(queue) {
   this._emitter = new EventEmitter();
   const self = this;
+  this.state = "running";
   queue.drain(() => {
     self._emitter.emit("toggle");
   });
@@ -10,16 +11,20 @@ function QueueManager(queue) {
 
 QueueManager.prototype.pause = function () {
   const self = this;
+  this.state = "paused";
   return new Promise((resolve) => {
     self._emitter.once("toggle", () => resolve(true));
   });
 };
 
 QueueManager.prototype.resume = function () {
+  if (this.state === "running") return;
   const ack = this._emitter.emit("toggle");
   if (!ack) {
     console.log(
       `resume event wasn't consumed, queue will resume after it's drained`
     );
+  } else {
+    this.state = "running";
   }
 };
